35c02056d335c7a617c39632b21b8f643f676197
renzisen <xj888xj@qq.com>
2022-09-04 08:13:15 +0800

    Update public.sh

--- meIsreallyba/scripts/public.sh
+++ meIsreallyba/scripts/public.sh
@@ -6,7 +6,7 @@
 
 user_name="admin"                                   # 用户名
 user_password="admin"                               # 登录密码
-lan_ip="192.168.2"                                  # LAN 地址 别写后面的 .1
+lan_ip="192.168.123"                                  # LAN 地址 别写后面的 .1
 wlan_2g_ssid="Padavan"                              # 2G 无线名称
 wlan_5g_ssid="Padavan_5G"                           # 5G 无线名称
 wlan_guest_2g_ssid="Padavan_Guset"                  # 2G 访客无线名称
